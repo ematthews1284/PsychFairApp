22601db554604be0302eecf702a165079ba119ea
period3 <period3@p-162mba-11.d214.org>
2018-10-30 10:27:14 -0500

    Connected the outlet for the Table View in the first view controller. Then connected the outlets for the Labels and Textfields in the 2nd view controller, while also connecting the submit button.

--- PsychFairApp/ViewController2.swift
+++ PsychFairApp/ViewController2.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class ViewController2: UIViewController {
-
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var TextField1: UITextField!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var TextField2: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
+    @IBAction func submitButton(_ sender: UIButton)
+    {
+        
+    }
+    
 
 }
