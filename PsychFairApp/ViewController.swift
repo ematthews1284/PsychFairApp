5c336c98b148059459afa8a3858083f16dcc224a
period3 <period3@p-162mba-10.d214.org>
2018-11-19 16:18:38 +0000

    uncommented import firebase, added segue, changed groups var, edited alert

--- PsychFairApp/ViewController.swift
+++ PsychFairApp/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var groups = [[String:String]]()
+    var groups: [String] = ["I need a place holder so yea here"]
+    
     let apiKey = "AIzaSyBuO2Z6d5C7B05MDnr-kpDfk-nbaG3m3qk"
     @IBOutlet weak var tableView: UITableView!
     
@@ -24,23 +25,23 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let source = groups[indexPath.row]
-        cell.textLabel?.text = source[""]
-        cell.detailTextLabel?.text = source[""]
+        cell.textLabel?.text = groups[indexPath.row]
+        //cell.detailTextLabel?.text =
         return cell
     }
-    func parse(json: JSON)
-    {
-        for result in json["groups"].arrayValue
-        {
-            let url = result[""].stringValue
-            let name = result[""].stringValue
-            let description = result[""].stringValue
-            let source = ["":description, "":name, "":url]
-            groups.append(source)
-        }
-        tableView.reloadData()
-    }
+    
+//    func parse(json: JSON)
+//    {
+//        for result in json["groups"].arrayValue
+//        {
+//            let url = result[""].stringValue
+//            let name = result[""].stringValue
+//            let description = result[""].stringValue
+//            let source = ["":description, "":name, "":url]
+//            groups.append(source)
+//        }
+//        tableView.reloadData()
+//    }
     
 }
 
